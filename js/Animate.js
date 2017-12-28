34984ad385afbcdcfd2538f15c18edaf7187b59f
Rocco Howard <rocco@hnh.digital>
2017-12-28 14:24:33 +1100

    Added ability to use a boolean to run animation, and also a false based animation

--- js/Animate.js
+++ js/Animate.js
@@ -3,10 +3,23 @@
  * Animate CSS.
  */
 $.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
+  animateCss: function (animationName, animate, animationFalse) {
+    animate = typeof animate !== 'undefined' ? animate : true;
+    animationFalse = typeof animationFalse !== 'undefined' ? animationFalse : true;
+
+
+    if (!animate && animationFalse != false) {
+      animate = true;
+      animationName = animationFalse;
     }
+
+    if (animate) {
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+        $(this).removeClass('animated ' + animationName);
+      });
+    }
+
+    return $(this);
+  }
 });
