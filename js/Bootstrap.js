bf282b7cab251ca0399712fcf0a299fd7ff3deae
Rocco Howard <rocco@hnh.digital>
2018-01-02 16:16:06 +1100

    Destory the tooltip

--- js/Bootstrap.js
+++ js/Bootstrap.js
@@ -4,6 +4,9 @@
  */
 $.frontendAssets.register('bs-tooltip', function(event) {
   $(this).data('toggle', 'tooltip');
+  if (typeof $(this).tooltip == 'function') {
+    $(this).tooltip('dispose');
+  }
   $(this).tooltip();
 
   $(this).trigger('extension::bs-tooltip::applied');
