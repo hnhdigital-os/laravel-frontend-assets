61c94b301776a52eee12e999dd101c85dcaf972b
Rocco Howard <rocco@hnh.digital>
2018-02-26 03:33:52 +0000

    Allow specified GA key

--- src/GoogleAnalytics.php
+++ src/GoogleAnalytics.php
@@ -6,19 +6,23 @@ use FrontendAsset;
 
 class GoogleAnalytics
 {
-    public function __construct()
+    public function __construct($ga_key = null)
     {
-        if (empty(env('GOOGLE_ANALYTICS_KEY'))) {
+        if (empty($ga_key) && !empty(env('GOOGLE_ANALYTICS_KEY'))) {
+            $ga_key = env('GOOGLE_ANALYTICS_KEY');
+        }
+
+        if (empty($ga_key)) {
             return;
         }
 
-        FrontendAsset::add('https://www.googletagmanager.com/gtag/js?id='.env('GOOGLE_ANALYTICS_KEY'));
+        FrontendAsset::add('https://www.googletagmanager.com/gtag/js?id='.$ga_key);
         // async
         
         $script = "window.dataLayer = window.dataLayer || [];\n";
         $script .= "function gtag(){dataLayer.push(arguments);}\n";
         $script .= "gtag('js', new Date());\n";
-        $script .= sprintf("gtag('config', '%s');", env('GOOGLE_ANALYTICS_KEY'));
+        $script .= sprintf("gtag('config', '%s');", $ga_key);
         FrontendAsset::addScript($script);
     }
 }
