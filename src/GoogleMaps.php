65da4ffae8c4eda87e855e4a8250b0f44718d177
Rocco Howard <rocco@hnh.digital>
2018-05-07 13:39:26 +0000

    Added marker clusterer extension

--- src/GoogleMaps.php
+++ src/GoogleMaps.php
@@ -3,11 +3,48 @@
 namespace HnhDigital\LaravelFrontendAssets;
 
 use FrontendAsset;
+use Illuminate\Support\Arr;
 
 class GoogleMaps
 {
-    public function __construct($version = false)
+    public function __construct($extension = false)
     {
         FrontendAsset::add('https://maps.google.com/maps/api/js?libraries=places&key='.env('GOOGLE_MAP_KEY'));
+
+        if ($extension !== false) {
+            $this->loadExtensions($extension);
+
+            return;
+        }
+    }
+
+    /**
+     * Load requested extensions.
+     *
+     * @param string|array $extensions
+     *
+     * @return void
+     */
+    private function loadExtensions($extensions)
+    {
+        $extensions = Arr::wrap($extensions);
+
+        foreach ($extensions as $extension) {
+            $extension = 'ext'.$extension;
+
+            if (method_exists($this, $extension)) {
+                $this->$extension();
+            }
+        }
+    }
+
+    /**
+     * Marker Clusterer.
+     *
+     * @return void
+     */
+    private function extMarkerClusterer()
+    {
+        FrontendAsset::add('https://cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer_compiled.js');
     }
 }
