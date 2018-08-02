87d61aba2427f6acb6fd1be77ee7eb77d3c02c2b
Rocco Howard <rocco@hnh.digital>
2018-08-02 13:38:30 +0000

    Improve text diff; Added applying trigger so can run with new content loaded

--- js/TextDiff.js
+++ js/TextDiff.js
@@ -4,25 +4,47 @@
 $.frontendAssets.register('text-diff', function(event) {
   var element = $(event.currentTarget);
 
-  var options = {
-    originalContent: element.find('.pretty-text-original').html(),
-    changedContent: element.find('.pretty-text-changed').html(),
-    diffContainer: element.find('.pretty-text-container'),
-  };
-  element.prettyTextDiff(options);
-  if (element.find('.pretty-text-original').html() == '') {
-    element.find('.pretty-text-original').html('<del>{empty value}</del>');
-  } else if (element.find('.pretty-text-changed').html() == '') {
-    element.find('.pretty-text-original').show();
-    element
-      .find('.pretty-text-original')
-      .html('<del>' + element.find('.pretty-text-original').html() + '</del><ins>{empty value}</ins>');
-    element.find('.pretty-text-changed').html('{empty value}');
-    element.find('.pretty-text-container').hide();
-  } else {
-    element.find('.pretty-text-original').hide();
-    element.find('.pretty-text-container').show();
-  }
+  element.on('apply', function() {
+    var container = $(this);
 
-  element.trigger('extension::text-diff::applied');
+    // Find the various content containers.
+    var original_content = container.find('.pretty-text-original');
+    var changed_content = container.find('.pretty-text-changed');
+    var diff_container = container.find('.pretty-text-container');
+
+    // Reset the content.
+    if (original_content.html().indexOf('{empty value}') >= 0) {
+      original_content.html('');
+    }
+
+    // Reset the content.
+    if (changed_content.html().indexOf('{empty value}') >= 0) {
+      changed_content.html('');
+    }
+
+    // Original content is empty.
+    if (changed_content.html() == '') {
+      changed_content.html('{empty value}');
+    }
+
+    // Hide the original content and show the diffs.
+    if (original_content.html() == '') {
+      original_content.html('{empty value}');
+    }
+
+    // Set the options for text-diff.
+    var options = {
+      originalContent: original_content.html(),
+      changedContent: changed_content.html(),
+      diffContainer: diff_container,
+    };
+
+    // Setup the diff.
+    container.prettyTextDiff(options);
+
+    original_content.hide();
+    diff_container.show();
+    container.trigger('extension::text-diff::applied');
+  }).trigger('apply');  
+
 });
