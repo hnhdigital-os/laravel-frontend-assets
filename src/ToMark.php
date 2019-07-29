c5e797adab8e58dda70c74b3af7e08a978f6f328
Rocco Howard <rocco@hnh.digital>
2019-07-29 01:40:40 +0000

    Fix url

--- src/ToMark.php
+++ src/ToMark.php
@@ -10,7 +10,7 @@ class ToMark
             app('FrontendAsset')->add('vendor/to-mark.js');
         } else {
             $version = app('FrontendAsset')->version(class_basename(__CLASS__), $version);
-            app('FrontendAsset')->add('https://uicdn.toast.com/to-mark/'.$version.'/to-mark.min.js');
+            app('FrontendAsset')->add('https://uicdn.toast.com/to-mark/v'.$version.'/to-mark.min.js');
         }
     }
 }
